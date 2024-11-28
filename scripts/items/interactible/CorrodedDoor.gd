50fd107d192ad98198ef52084e19e15b1dbe2992
reese <reeseyh18@gmail.com>
2024-11-27 19:19:54 -0500

    doors work

---
+++ scripts/items/interactible/CorrodedDoor.gd
@@ -0,0 +1,60 @@
+### CorrodedDoor.gd
+
+extends StaticBody2D
+
+var player_near = false
+var player_body = null
+var door_interact = false
+
+@export var messages : Array = []
+@export var tess_messages : Array = []
+@onready var dialog_popup = $LongerDescPopup
+@onready var lights_off = $"../Room2_Lights"
+
+var current_message_index : int = 0
+
+func _on_area_2d_body_entered(body: Node2D):
+	if body.is_in_group("active"):
+		player_near = true
+		player_body = body
+
+func _on_area_2d_body_exited(body: Node2D):
+	if body == player_body:
+		player_near = false
+		player_body = null
+
+func _process(delta: float):
+	if player_body and player_near and Input.is_action_just_pressed("ui_interact"):
+		if player_body.name == "Tess":
+			if current_message_index < tess_messages.size():
+				dialog_popup.message_set(tess_messages[current_message_index])
+				dialog_popup.open()
+				current_message_index += 1
+			else:
+				dialog_popup.close()
+				current_message_index = 0
+				$CorrodedDoor.modulate.a = 0.3
+				$CollisionShape2D.disabled = true
+				lights_off.visible = false
+				_crawl_through_door()
+		elif player_body.name in ["Charlie", "Jay"]:
+			if current_message_index < messages.size():
+				dialog_popup.message_set(messages[current_message_index])
+				dialog_popup.open()
+				current_message_index += 1
+			else:
+				dialog_popup.close()
+				current_message_index = 0
+
+
+func _crawl_through_door():
+	var countdown = Timer.new()
+	add_child(countdown)
+	countdown.wait_time = 3.0
+	countdown.one_shot = true 
+	countdown.autostart = false
+	countdown.start()
+	
+	await countdown.timeout
+	$CorrodedDoor.modulate.a = 1
+	$CollisionShape2D.disabled = false
